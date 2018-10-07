b977f43e817affa3175f6a6e4d33c97dbf452e8b
Maximilian Brandau <mbrandau@online.de>
2018-10-07 16:25:10 +0200

    Add prettier Update dependencies

--- test/test.js
+++ test/test.js
@@ -29,11 +29,15 @@ describe('ip-blacklist', function() {
     });
     it('should call next middleware', function(done) {
       const middleware = ipBlacklist(ipBlacklist.array(['127.0.0.1']));
-      middleware({
-        ip: '127.0.0.2'
-      }, {}, function() {
-        done();
-      });
+      middleware(
+        {
+          ip: '127.0.0.2'
+        },
+        {},
+        function() {
+          done();
+        }
+      );
     });
     it('should end request with code 403', function(done) {
       const middleware = ipBlacklist(ipBlacklist.array(['127.0.0.1']));
@@ -45,40 +49,62 @@ describe('ip-blacklist', function() {
           done();
         }
       };
-      middleware({
-        ip: '127.0.0.1'
-      }, res, function() {
-        expect().fail('Should not call next() here');
-      });
-    })
+      middleware(
+        {
+          ip: '127.0.0.1'
+        },
+        res,
+        function() {
+          expect().fail('Should not call next() here');
+        }
+      );
+    });
   });
   describe('#array()', function() {
-    const cb = ipBlacklist.array(['127.0.0.1', '::1', 'i am not an ip address']);
-    it('should return true when the ip is 127.0.0.1', () => assert.equal(true, cb('127.0.0.1')));
-    it('should return true when the ip is ::1', () => assert.equal(true, cb('::1')));
-    it('should return false when the ip is 127.0.0.2', () => assert.equal(false, cb('127.0.0.2')));
-    it('should return false when the ip is ::2', () => assert.equal(false, cb('::2')));
+    const cb = ipBlacklist.array([
+      '127.0.0.1',
+      '::1',
+      'i am not an ip address'
+    ]);
+    it('should return true when the ip is 127.0.0.1', () =>
+      assert.equal(true, cb('127.0.0.1')));
+    it('should return true when the ip is ::1', () =>
+      assert.equal(true, cb('::1')));
+    it('should return false when the ip is 127.0.0.2', () =>
+      assert.equal(false, cb('127.0.0.2')));
+    it('should return false when the ip is ::2', () =>
+      assert.equal(false, cb('::2')));
     it('should filter out invalid IP addresses', function() {
       expect(cb('i am not an ip address')).to.be(false);
     });
   });
   describe('#file()', function() {
     const cb = ipBlacklist.file(path.join(__dirname, 'ips.txt'));
-    it('should return true when the ip is 127.0.0.1', () => assert.equal(true, cb('127.0.0.1')));
-    it('should return true when the ip is ::1', () => assert.equal(true, cb('::1')));
-    it('should return false when the ip is 127.0.0.2', () => assert.equal(false, cb('127.0.0.2')));
-    it('should return false when the ip is ::2', () => assert.equal(false, cb('::2')));
+    it('should return true when the ip is 127.0.0.1', () =>
+      assert.equal(true, cb('127.0.0.1')));
+    it('should return true when the ip is ::1', () =>
+      assert.equal(true, cb('::1')));
+    it('should return false when the ip is 127.0.0.2', () =>
+      assert.equal(false, cb('127.0.0.2')));
+    it('should return false when the ip is ::2', () =>
+      assert.equal(false, cb('::2')));
   });
   describe('#chain()', () => {
     const cb = ipBlacklist.chain(
       ipBlacklist.file(path.join(__dirname, 'ips.txt')),
       ipBlacklist.array(['127.0.0.3', '::3'])
     );
-    it('should return true when the ip is 127.0.0.1', () => assert.equal(true, cb('127.0.0.1')));
-    it('should return true when the ip is ::1', () => assert.equal(true, cb('::1')));
-    it('should return false when the ip is 127.0.0.2', () => assert.equal(false, cb('127.0.0.2')));
-    it('should return false when the ip is ::2', () => assert.equal(false, cb('::2')));
-    it('should return true when the ip is 127.0.0.3', () => assert.equal(true, cb('127.0.0.3')));
-    it('should return true when the ip is ::3', () => assert.equal(true, cb('::3')));
+    it('should return true when the ip is 127.0.0.1', () =>
+      assert.equal(true, cb('127.0.0.1')));
+    it('should return true when the ip is ::1', () =>
+      assert.equal(true, cb('::1')));
+    it('should return false when the ip is 127.0.0.2', () =>
+      assert.equal(false, cb('127.0.0.2')));
+    it('should return false when the ip is ::2', () =>
+      assert.equal(false, cb('::2')));
+    it('should return true when the ip is 127.0.0.3', () =>
+      assert.equal(true, cb('127.0.0.3')));
+    it('should return true when the ip is ::3', () =>
+      assert.equal(true, cb('::3')));
   });
 });
